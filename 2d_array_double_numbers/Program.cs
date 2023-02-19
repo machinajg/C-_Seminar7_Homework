97f542019b99c429d484ee42d75bd1e3eea3e66f
Julia Machina <machinajg@gmail.com>
2023-02-20 02:41:49 +0300

    2D array filled with double numbers project added

--- 2d_array_double_numbers/Program.cs
+++ 2d_array_double_numbers/Program.cs
@@ -4,30 +4,29 @@
 // 1 -3,3 8 -9,9
 // 8 7,8 -7,1 9
 
+double[,] array = new double[5,4]; 
 
-
-void FillArray(double[,] Number)     //Заполняем массив случайными цифрами
+void FillArray(double[,] Number)   //Заполняем массив случайными вещественными цифрами
 {
     for (int m = 0; m < Number.GetLength(0); m++)
     {
         for (int n = 0; n < Number.GetLength(1); n++)
         {
-            Number[m,n] =   new Random().Next(-100,101);  
+            Number[m,n] = Convert.ToDouble(new Random().Next(-100, 100)/10.0);}
         }            
     }
-}
 void PrintArray(double[,] printnumb)       //Выводим массив на экран
 {
     for (int m = 0; m < printnumb.GetLength(0); m++)
     {
         for (int n = 0; n < printnumb.GetLength(1); n++)
         {
-            Console.Write($"{printnumb[m,n]} ");
+            double pri = Math.Round(printnumb[m,n],1);
+            Console.Write($"{pri} ");
         }
     Console.WriteLine();
     }    
 }
-double[,] array = new double[3,4];
 FillArray(array);
 PrintArray(array);
 
